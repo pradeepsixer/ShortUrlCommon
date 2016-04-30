7555679a422ded7f04ce0087377a6678f5a87eff
Pradeep Kumar <pradeepsixer@gmail.com>
2016-04-30 19:55:28 +0530

    1. Created a String Constants Class. 2. Created Private Constructors to prevent Instantiation

--- src/com/shorturl/common/CommonConstants.java
+++ src/com/shorturl/common/CommonConstants.java
@@ -1,15 +1,31 @@
 package com.shorturl.common;
 
 public class CommonConstants {
+	private CommonConstants() {
+	}
+
 	public static class NumericConstants {
+		private NumericConstants() {
+		}
+
 		public static final int ZERO = 0;
 	}
 
 	public static class CharacterConstants {
+		private CharacterConstants() {
+		}
+
 		// Digits go here
 		public static final char ZERO = '0';
 
 		// small alphabets go here
 		public static final char SMALL_A = 'a';
+	}
+
+	public static class StringConstants {
+		private StringConstants() {
+		}
+
+		public static final String EMPTY_STRING = "";
 	}
 }
